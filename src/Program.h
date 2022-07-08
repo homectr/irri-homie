9dfc09d59455c428ce5336cef124f3f248ca6b71
Jozef Sovcik <jozef@sovcik.net>
2022-07-08 14:13:27 +0200

    code formatting

--- src/Program.h
+++ src/Program.h
@@ -3,145 +3,149 @@
 #include <TimeLib.h>
 #include <Homie.h>
 #include "settings.h"
-#include "Valve.h" 
+#include "Valve.h"
 
 class Program;
 
-using program_cb_t = void(*)(Program* program);
+using program_cb_t = void (*)(Program *program);
 
-class Program {
+class Program
+{
 
-    private:
-        // program identifier
-        unsigned char id;
+private:
+  // program identifier
+  unsigned char id;
 
-        // program identifer for mqtt
-        char* idStr = NULL;
+  // program identifer for mqtt
+  char *idStr = NULL;
 
-        // program starting hour
-        unsigned char startHour = 6;
+  // program starting hour
+  unsigned char startHour = 6;
 
-        // program starting minute
-        unsigned char startMin = 0;
+  // program starting minute
+  unsigned char startMin = 0;
 
-        // how long (in seconds) should each valve be open
-        unsigned int runTimes[NUMBER_OF_VALVES];
+  // how long (in seconds) should each valve be open
+  unsigned int runTimes[NUMBER_OF_VALVES];
 
-        // which days of a week should program run, starting with Sunday
-        unsigned char runDays[7] = {0,0,0,0,0,0,0};
+  // which days of a week should program run, starting with Sunday
+  unsigned char runDays[7] = {0, 0, 0, 0, 0, 0, 0};
 
-        // array of pointers to valves
-        Valve* valves[NUMBER_OF_VALVES];
+  // array of pointers to valves
+  Valve *valves[NUMBER_OF_VALVES];
 
-        // count of valves 
-        unsigned char valveCount = 0;
+  // count of valves
+  unsigned char valveCount = 0;
 
-        // 1=program is running
-        unsigned char status = 0; 
+  // 1=program is running
+  unsigned char status = 0;
 
-        // currently used valve
-        unsigned char currentValve = 0;
+  // currently used valve
+  unsigned char currentValve = 0;
 
-        // valve runtime multiplier, e.g. 120 = 120% valve runtime
-        unsigned char intensity = 100;
+  // valve runtime multiplier, e.g. 120 = 120% valve runtime
+  unsigned char intensity = 100;
 
-        // callback to be called when program starts
-        program_cb_t onStart = NULL;
+  // callback to be called when program starts
+  program_cb_t onStart = NULL;
 
-        // callback to be called when program stops
-        program_cb_t onStop = NULL;
+  // callback to be called when program stops
+  program_cb_t onStop = NULL;
 
-        // Homie node
-        HomieNode* homie = NULL;
+  // Homie node
+  HomieNode *homie = NULL;
 
-    public:
-        /**
-         * Create new irrigation program. Program are scheduling valves.
-         * 
-         * @param id - program identified. Used as Homie node identifier.
-         */
-        Program(unsigned char id);
+public:
+  /**
+   * Create new irrigation program. Program are scheduling valves.
+   *
+   * @param id - program identified. Used as Homie node identifier.
+   */
+  Program(unsigned char id);
 
-        // returns program identifier
-        const char* getIdStr(){return idStr;};
+  // returns program identifier
+  const char *getIdStr() { return idStr; };
 
-        // Starts program.
-        void start();
+  // Starts program.
+  void start();
 
-        // Stops program.
-        void stop();
+  // Stops program.
+  void stop();
 
-        // IMPORTANT: Program internal loop. Must be called in the main loop.
-        void loop();
+  // IMPORTANT: Program internal loop. Must be called in the main loop.
+  void loop();
 
-        // Sets program start time
-        void setStart(unsigned char hour, unsigned char minute){startHour = hour; startMin = minute;};
+  // Sets program start time
+  void setStart(unsigned char hour, unsigned char minute)
+  {
+    startHour = hour;
+    startMin = minute;
+  };
 
-        // Returns programs start hour.
-        unsigned char getStartHour(){return startHour;};
+  // Returns programs start hour.
+  unsigned char getStartHour() { return startHour; };
 
-        // Returns program start minute.
-        unsigned char getStartMinute(){return startMin;};
+  // Returns program start minute.
+  unsigned char getStartMinute() { return startMin; };
 
-        /**
-         * Sets runtime for specified valve.
-         * 
-         * @param valve - valve number
-         * @param runtime - valve runtime in seconds
-         */
-        unsigned char setRunTime(unsigned char valve, unsigned int runtime);
+  /**
+   * Sets runtime for specified valve.
+   *
+   * @param valve - valve number
+   * @param runtime - valve runtime in seconds
+   */
+  unsigned char setRunTime(unsigned char valve, unsigned int runtime);
 
-        // Sets runtime of specified valve (in minutes)
-        unsigned char setRunTimeMin(unsigned char valve, unsigned int runtime){setRunTime(valve,runtime*60);};
+  // Sets runtime of specified valve (in minutes)
+  unsigned char setRunTimeMin(unsigned char valve, unsigned int runtime) { return setRunTime(valve, runtime * 60); };
 
-        // Returns runtime of specified valve (in seconds)
-        unsigned int getRunTime(unsigned char valve);
+  // Returns runtime of specified valve (in seconds)
+  unsigned int getRunTime(unsigned char valve);
 
-        // Returns runtime of specified valve (in minutes)
-        unsigned int getRunTimeMin(unsigned char valve){return getRunTime(valve)/60;};
+  // Returns runtime of specified valve (in minutes)
+  unsigned int getRunTimeMin(unsigned char valve) { return getRunTime(valve) / 60; };
 
-        /**
-         * Toggles specified day of week.
-         * 
-         * @param day - day of week. Sunday = 1, Monday = 2, ...
-         * @param status - true = program runs on that day of week
-         */
-        unsigned char setRunDay(unsigned char day, bool status);
+  /**
+   * Toggles specified day of week.
+   *
+   * @param day - day of week. Sunday = 1, Monday = 2, ...
+   * @param status - true = program runs on that day of week
+   */
+  unsigned char setRunDay(unsigned char day, bool status);
 
-        // Returns true if program runs on specified day of week
-        unsigned char getRunDay(unsigned char day);
+  // Returns true if program runs on specified day of week
+  unsigned char getRunDay(unsigned char day);
 
-        // Sets program intensity
-        void setIntensity(unsigned char i){intensity = i;};
+  // Sets program intensity
+  void setIntensity(unsigned char i) { intensity = i; };
 
-        // Sets callback for program on-start event
-        void setOnStartCB(program_cb_t cb){onStart = cb;};
+  // Sets callback for program on-start event
+  void setOnStartCB(program_cb_t cb) { onStart = cb; };
 
-        // Sets callback for program on-stop event
-        void setOnStopCB(program_cb_t cb){onStop = cb;};
-        
-        // Determine if program should start on specified datetime
-        unsigned char shouldStart(time_t datetime);
+  // Sets callback for program on-stop event
+  void setOnStopCB(program_cb_t cb) { onStop = cb; };
 
-        // Returns true if program is currently running
-        unsigned char isRunning(){return status;};
+  // Determine if program should start on specified datetime
+  unsigned char shouldStart(time_t datetime);
 
-        // Adds valve to program
-        void addValve(Valve* valve);
+  // Returns true if program is currently running
+  unsigned char isRunning() { return status; };
 
-        /**
-         * Returns true if program is configured.
-         * Configured means that program runs at least once per week 
-         * and at least one valve runtime is non-zero.
-         */
-        unsigned char isConfigured();
+  // Adds valve to program
+  void addValve(Valve *valve);
 
-        void printConfig();
+  /**
+   * Returns true if program is configured.
+   * Configured means that program runs at least once per week
+   * and at least one valve runtime is non-zero.
+   */
+  unsigned char isConfigured();
 
-        // Set Homie node representing this program
-        void setHomie(HomieNode* node){homie = node;}
+  void printConfig();
 
-        // Return Homie node representing this program
-        inline HomieNode* getHomie(){return homie;}
+  // Set Homie node representing this program
+  void setHomie(HomieNode *node) { homie = node; }
 
+  // Return Homie node representing this program
+  inline HomieNode *getHomie() { return homie; }
 };
--- src/config.cpp
+++ src/config.cpp
@@ -4,10 +4,10 @@
 
 #ifdef USE_LITTLE_FS
 #include <LittleFS.h>
-#define BOARD_FS    LittleFS
+#define BOARD_FS LittleFS
 #else
 #include <FS.h>
-#define BOARD_FS    SPIFFS
+#define BOARD_FS SPIFFS
 #endif
 
 #include <TimeLib.h>
@@ -23,8 +23,8 @@
 #define NODEBUG_PRINT
 #include "debug_print.h"
 
-extern Valve* valves[NUMBER_OF_VALVES];
-extern Program* programs[NUMBER_OF_PROGRAMS];
+extern Valve *valves[NUMBER_OF_VALVES];
+extern Program *programs[NUMBER_OF_PROGRAMS];
 
 extern time_t sys_disabledTill;
 extern char sys_disabledTillStr[25];
@@ -32,17 +32,20 @@ extern char sys_disabledTillStr[25];
 extern unsigned char sys_intensity;
 
 //------------ FS
-int initFS(){
-    CONSOLE_PGM(PSTR("[init:FS] Mounting FS\n"));
-    if (!BOARD_FS.begin()){
-        CONSOLE_PGM(PSTR("[init:FS] >  FS FAILED TO MOUNT!\n"));
-        CONSOLE_PGM(PSTR("[init:FS] >  Make sure to upload config.json to FS first!\n"));
-    } else {
-        DEBUG_PRINT("[init:FS] FS available\n");
-    }
-    return 1;
+int initFS()
+{
+  CONSOLE_PGM(PSTR("[init:FS] Mounting FS\n"));
+  if (!BOARD_FS.begin())
+  {
+    CONSOLE_PGM(PSTR("[init:FS] >  FS FAILED TO MOUNT!\n"));
+    CONSOLE_PGM(PSTR("[init:FS] >  Make sure to upload config.json to FS first!\n"));
+  }
+  else
+  {
+    DEBUG_PRINT("[init:FS] FS available\n");
+  }
+  return 1;
 }
-
 
 /**
  * Load configuration stored in EEPROM (BOARD_FS).
@@ -50,7 +53,7 @@ int initFS(){
  * {
  *    irrigation: {
  *      intensity: 100,
- *      disabled-till: "YYYY-MM-DD"
+ *      disabled-till: 0,
  *      valves:[10,10,0,0,0,0],
  *      programs:[
  *        { id: 1, run-times:"10,10,20,0,0,0", run-days:"10010101", start-hour: 6, start-min: 30 }
@@ -58,162 +61,177 @@ int initFS(){
  *    }
  * }
  */
-int loadConfig() {
-    const char *module = "[init:ldcfg]";
-    DynamicJsonDocument jdoc(2048);
+int loadConfig()
+{
+  const char *module = "[init:ldcfg]";
+  DynamicJsonDocument jdoc(2048);
 
-    const char* f = LOCAL_CONFIG_FILE;
-    DEBUG_PRINT("%s reading local config file=%s\n", module, f);
-    
-    if (!BOARD_FS.exists(f)) {
-        DEBUG_PRINT("%s no configuration file\n", module);
-        return 0;
-    }
-    
-    File file = BOARD_FS.open(f,"r");
-    auto err = deserializeJson(jdoc,file);
-    file.close();
+  const char *f = LOCAL_CONFIG_FILE;
+  DEBUG_PRINT("%s reading local config file=%s\n", module, f);
 
-    if (err){
-        CONSOLE_PGM(PSTR("%s invalid configuration file. error=%s\n"), module, err.c_str());
-        return 0;
-    }
+  if (!BOARD_FS.exists(f))
+  {
+    DEBUG_PRINT("%s no configuration file\n", module);
+    return 0;
+  }
 
-    JsonVariant jv;
-    JsonArray ja;
-    JsonObject jroot = jdoc[F("settings")];
-    unsigned int i = 0;
+  File file = BOARD_FS.open(f, "r");
+  auto err = deserializeJson(jdoc, file);
+  file.close();
 
-    // read valves
-    CONSOLE_PGM(PSTR("%s Reading valves\n"), module);
-    ja = jroot[F("valves")].as<JsonArray>();
-    for(JsonVariant v : ja) {
-        if (i>=NUMBER_OF_VALVES) {
-            CONSOLE_PGM(PSTR("%s Error: too many valves in configuration file\n"), module);
-            break;
-        }
-        CONSOLE_PGM(PSTR("%s   Valve %d "), module, i);
-        unsigned int rt = v | 0;
-        valves[i]->setRunTime(rt);
-        CONSOLE_PGM(PSTR("runtime=%d\n"), rt);
-        i++;
-    }
+  if (err)
+  {
+    CONSOLE_PGM(PSTR("%s invalid configuration file. error=%s\n"), module, err.c_str());
+    return 0;
+  }
 
-    // read programs
-    CONSOLE_PGM(PSTR("%s Reading programs\n"), module);
-    ja = jroot[F("programs")].as<JsonArray>();
-    i = 0;
-    for(JsonVariant prg : ja) {
-        if (i>=NUMBER_OF_PROGRAMS) {
-            CONSOLE_PGM(PSTR("%s Error: too many programs in configuration file\n"), module);
-            break;
-        }
+  JsonVariant jv;
+  JsonArray ja;
+  JsonObject jroot = jdoc[F("settings")];
+  unsigned int i = 0;
 
-        CONSOLE_PGM(PSTR("%s   Program %d "), module, i);
-        CONSOLE(" rt=");
-        int ii=0;
-        for (JsonVariant vrt: prg[F("run-times")].as<JsonArray>()){
-            unsigned int rt = vrt | 0;
-            programs[i]->setRunTime(ii,rt);
-            CONSOLE("%d,",programs[i]->getRunTime(ii));
-            ii++;
-            
-        }
-
-        ii=0;
-        CONSOLE(" rd=");
-        for (JsonVariant vrd: prg[F("run-days")].as<JsonArray>()){
-            unsigned char rd = vrd | 0;
-            programs[i]->setRunDay(ii,rd);
-            CONSOLE("%d:",programs[i]->getRunDay(ii));
-            ii++;
-            
-        }
-
-        programs[i]->setStart(
-            prg[F("start-hour")] | (unsigned char)6,
-            prg[F("start-min")] | (unsigned char)0
-        );
-
-        CONSOLE(" start=%02d:%02d\n",programs[i]->getStartHour(), programs[i]->getStartMinute());
-        i++;
-    }
-
-    CONSOLE_PGM(PSTR("%s Reading global settings\n"), module);
+  // read valves
+  CONSOLE_PGM(PSTR("%s Reading valves\n"), module);
+  ja = jroot[F("valves")].as<JsonArray>();
+  for (JsonVariant v : ja)
+  {
+    if (i >= NUMBER_OF_VALVES)
     {
-        // read intensity
-        unsigned char i = jroot[F("intensity")] | (unsigned char)100;
-        CONSOLE_PGM(PSTR("%s   Intensity=%d\n"), module, i);
-        sys_intensity = i;
-        for(int p=0;p<NUMBER_OF_PROGRAMS;p++) programs[p]->setIntensity(i);
+      CONSOLE_PGM(PSTR("%s Error: too many valves in configuration file\n"), module);
+      break;
     }
- 
+    CONSOLE_PGM(PSTR("%s   Valve %d "), module, i);
+    unsigned int rt = v | 0;
+    valves[i]->setRunTime(rt);
+    CONSOLE_PGM(PSTR("runtime=%d\n"), rt);
+    i++;
+  }
+
+  // read programs
+  CONSOLE_PGM(PSTR("%s Reading programs\n"), module);
+  ja = jroot[F("programs")].as<JsonArray>();
+  i = 0;
+  for (JsonVariant prg : ja)
+  {
+    if (i >= NUMBER_OF_PROGRAMS)
     {
-        // read disabled till
-        time_t i = jroot[F("disabled-till")] | 0;
-        sys_disabledTill = i;
-        dt2ISO(sys_disabledTillStr,25,sys_disabledTill,false,NULL);
-        CONSOLE_PGM(PSTR("%s   Disabled till=%d\n"), module, i);
+      CONSOLE_PGM(PSTR("%s Error: too many programs in configuration file\n"), module);
+      break;
     }
-    
-    return 1;
+
+    CONSOLE_PGM(PSTR("%s   Program %d "), module, i);
+    CONSOLE(" rt=");
+    int ii = 0;
+    for (JsonVariant vrt : prg[F("run-times")].as<JsonArray>())
+    {
+      unsigned int rt = vrt | 0;
+      programs[i]->setRunTime(ii, rt);
+      CONSOLE("%d,", programs[i]->getRunTime(ii));
+      ii++;
+    }
+
+    ii = 0;
+    CONSOLE(" rd=");
+    for (JsonVariant vrd : prg[F("run-days")].as<JsonArray>())
+    {
+      unsigned char rd = vrd | 0;
+      programs[i]->setRunDay(ii, rd);
+      CONSOLE("%d:", programs[i]->getRunDay(ii));
+      ii++;
+    }
+
+    programs[i]->setStart(
+        prg[F("start-hour")] | (unsigned char)6,
+        prg[F("start-min")] | (unsigned char)0);
+
+    CONSOLE(" start=%02d:%02d\n", programs[i]->getStartHour(), programs[i]->getStartMinute());
+    i++;
+  }
+
+  CONSOLE_PGM(PSTR("%s Reading global settings\n"), module);
+  {
+    // read intensity
+    unsigned char i = jroot[F("intensity")] | (unsigned char)100;
+    CONSOLE_PGM(PSTR("%s   Intensity=%d\n"), module, i);
+    sys_intensity = i;
+    for (int p = 0; p < NUMBER_OF_PROGRAMS; p++)
+      programs[p]->setIntensity(i);
+  }
+
+  {
+    // read disabled till
+    time_t i = jroot[F("disabled-till")] | 0;
+    sys_disabledTill = i;
+    dt2ISO(sys_disabledTillStr, 25, sys_disabledTill, false, NULL);
+    CONSOLE_PGM(PSTR("%s   Disabled till=%d\n"), module, i);
+  }
+
+  return 1;
 }
 
-int saveConfig(){
-    const char *module = "[init:svcfg]";
-    DynamicJsonDocument jdoc(3000);
+int saveConfig()
+{
+  const char *module = "[init:svcfg]";
+  DynamicJsonDocument jdoc(3000);
 
-    const char* f = LOCAL_CONFIG_FILE;
-    File file = BOARD_FS.open(f,"r");
-    auto err = deserializeJson(jdoc,file);
-    file.close();
+  const char *f = LOCAL_CONFIG_FILE;
+  File file = BOARD_FS.open(f, "r");
+  auto err = deserializeJson(jdoc, file);
+  file.close();
 
-    if (err){
-        CONSOLE_PGM(PSTR("%s invalid configuration file. error=%s\n"), module, err.c_str());
-        // FIXME ignoring error and writing config anyway...
-    }    
-    
-    DEBUG_PRINT("%s Preparing local config\n", module);
-    jdoc.remove("settings");
-    JsonObject jroot = jdoc.createNestedObject("settings");
+  if (err)
+  {
+    CONSOLE_PGM(PSTR("%s invalid configuration file. error=%s\n"), module, err.c_str());
+    // FIXME ignoring error and writing config anyway...
+  }
 
-    if (valves[0]) {
-        JsonArray ja = jroot.createNestedArray("valves");
-        for(int i=0;i<NUMBER_OF_VALVES;i++)
-            ja.add(valves[i]->getRunTime());
+  DEBUG_PRINT("%s Preparing local config\n", module);
+  jdoc.remove("settings");
+  JsonObject jroot = jdoc.createNestedObject("settings");
+
+  if (valves[0])
+  {
+    JsonArray ja = jroot.createNestedArray("valves");
+    for (int i = 0; i < NUMBER_OF_VALVES; i++)
+      ja.add(valves[i]->getRunTime());
+  }
+
+  if (programs[0])
+  {
+    JsonArray ja = jroot.createNestedArray("programs");
+    for (int i = 0; i < NUMBER_OF_PROGRAMS; i++)
+    {
+      if (programs[i]->isConfigured())
+      {
+        JsonObject jb = ja.createNestedObject();
+        jb["start-hour"] = programs[i]->getStartHour();
+        jb["start-min"] = programs[i]->getStartMinute();
+
+        JsonArray ja = jb.createNestedArray("run-times");
+        for (int j = 0; j < NUMBER_OF_VALVES; j++)
+          ja.add(programs[i]->getRunTime(j));
+
+        ja = jb.createNestedArray("run-days");
+        for (int j = 0; j < 7; j++)
+          ja.add(programs[i]->getRunDay(j));
+      }
     }
+  }
 
-    if (programs[0]) {
-        JsonArray ja = jroot.createNestedArray("programs");
-        for(int i=0;i<NUMBER_OF_PROGRAMS;i++){
-            if (programs[i]->isConfigured()) {
-                JsonObject jb = ja.createNestedObject();
-                jb["start-hour"] = programs[i]->getStartHour();
-                jb["start-min"] = programs[i]->getStartMinute();
+  jroot["intensity"] = sys_intensity;
+  jroot["disabled-till"] = sys_disabledTill;
 
-                JsonArray ja = jb.createNestedArray("run-times");
-                for(int j=0; j<NUMBER_OF_VALVES;j++) ja.add(programs[i]->getRunTime(j));
+  DEBUG_PRINT("%s Saving local config file=%s\n", module, f);
+  file = BOARD_FS.open(f, "w");
+  size_t size = serializeJson(jdoc, file);
+  DEBUG_PRINT("%s Saved %d bytes\n", module, size);
+  file.close();
 
-                ja = jb.createNestedArray("run-days");
-                for(int j=0; j<7; j++) ja.add(programs[i]->getRunDay(j));
-            }
-        }
-    }
-    
-    jroot["intensity"] = sys_intensity;
-    jroot["disabled-till"] = sys_disabledTill;
+#ifndef NODEBUG_PRINT
+  DEBUG_PRINT("Saved: start>>\n");
+  serializeJsonPretty(jroot, Serial);
+  DEBUG_PRINT("<<end\n");
+#endif
 
-    DEBUG_PRINT("%s Saving local config file=%s\n", module, f);
-    file = BOARD_FS.open(f,"w");
-    size_t size = serializeJson(jdoc,file);
-    DEBUG_PRINT("%s Saved %d bytes\n", module, size);
-    file.close();
-
-    #ifndef NODEBUG_PRINT
-    DEBUG_PRINT("Saved: start>>\n");
-    serializeJsonPretty(jroot,Serial);
-    DEBUG_PRINT("<<end\n");
-    #endif
-
-    return 1;
+  return 1;
 }
